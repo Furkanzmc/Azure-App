6c3be616fd5fa82e2a080978834f78603381c054
Furkan Üzümcü <furkan.uzumcu@edmoware.com>
2016-09-12 02:58:36 +0300

    Add rest framework test

--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
 
 def index(request):
     return HttpResponse("Hello there! Remember to Don't fix bug!")
+
+
+class TestAPIView(APIView):
+
+    def get(self, request, format=None):
+        return Response({"it": "works!"}, status=status.HTTP_200_OK)
